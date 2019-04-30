1d24083cfd4d7c594530bd223e6459b7d6579b41
62kuto1bvg <62kuto1bvg@hft-stuttgart.de>
2019-04-30 15:29:45 +0200

    Bearbeitung Breitengrade

--- IGP-ProjektAufgabe4/src/Client/Koordinatengitter.java
+++ IGP-ProjektAufgabe4/src/Client/Koordinatengitter.java
@@ -19,6 +19,9 @@ public class Koordinatengitter extends JPanel {
 	ArrayList<Integer> Laengengradetransformiert = new ArrayList<>();
 	ArrayList<Double> Laengengrade = new ArrayList<>();
 
+	ArrayList<Integer> Breitengradetransformiert = new ArrayList<>();
+	ArrayList<Double> Breitengrade = new ArrayList<>();
+
 	public void erzeugeKoordinatengitter(String crs, double minx, double miny, double maxx, double maxy,
 			double verhaeltnis, int width) {
 
@@ -48,21 +51,31 @@ public class Koordinatengitter extends JPanel {
 
 		// Winkel Bogensegment
 		double beta = ((maxx - minx));
-
+		// winkelzwischen den breitengrade
+		double alpha = (maxy - miny);
+		// iterationsverte, um winkel, welche kleiner sind als 1 zu verinfachen
 		double betaVereinfacht = 0;
 		double betaiteration = beta;
 		int betaiterationcounter = 0;
 
+		double alphaVereinfacht = 0;
+		double alphaiteration = alpha;
+		int alphaiterationcounter = 0;
+
 		double KoordinatengitterStartxiteration = minx;
 		double KoordinatengitterStartxVereinfacht = 0;
-		System.out.println("beta " + beta);
+
+		double KoordinatengitterStartyiteration = minx;
+		double KoordinatengitterStartyVereinfacht = 0;
 
 		// der winkel beta muss aehnlich wie bei der Massstabsleiste vereinfacht werden.
 		// Hierfuer muss ersteinmal ueberprueft werden, ob diese groesser oder kleiner
 		// als 1Grad ist
-		// fuer beta muss ein iterationshilfswert bbenutzt werden
+		// fuer beta muss ein iterationshilfswert benutzt werden
+		// zusätzlich wird ein Counter
 
 		///////////////////////////////////////////////////////////////////////////////////////////////////
+
 		if (betaiteration < 1.00) {
 
 			do {
@@ -70,32 +83,28 @@ public class Koordinatengitter extends JPanel {
 
 				betaiterationcounter = betaiterationcounter + 1;
 
-				System.out.println("betaiteration " + betaiteration);
-				System.out.println("betaiterationcounter " + betaiterationcounter);
-
 				// zur vereinfachung wird die zahl gerundet-
 
 				double betaiterationVereinfacht = Math.round(betaiteration);
-				System.out.println("KLEINERE  betaiterationVereinfacht " + betaiterationVereinfacht);
+
 				// und nun wieder in die Ursprungliche dimension umgewandelt
 
 				if (betaiterationcounter == 0) {
-
 					betaVereinfacht = betaiterationVereinfacht;
-
 				}
 
 				else {
 					betaVereinfacht = betaiterationVereinfacht / Math.pow(10, betaiterationcounter);
 				}
-				System.out.println("KLEINERE  betaVereinfacht " + betaVereinfacht);
+
 				// somit wurden zumbeispiel aus 0,337854 Grad jetzt 0.3grad generiert
-				System.out.println("betaVereinfacht " + betaVereinfacht);
 
 			} while (betaiteration <= 1);
 
-			// Ausserdem wird noch ein startwert benoetigt um von diesm die koordinatenwerte
+			// Ausserdem wird noch ein startwert benoetigt um von diesem die
+			// koordinatenwerte
 			// abzutragen
+
 			KoordinatengitterStartxiteration = KoordinatengitterStartxiteration * Math.pow(10, betaiterationcounter);
 
 			double iterationKoordinatengitterStartxVereinfacht = Math.round(KoordinatengitterStartxiteration);
@@ -103,17 +112,55 @@ public class Koordinatengitter extends JPanel {
 			KoordinatengitterStartxVereinfacht = iterationKoordinatengitterStartxVereinfacht
 					/ Math.pow(10, betaiterationcounter);
 
-			System.out.println("min x " + minx);
-			System.out.println("KoordinatengitterStartxVereinfacht x " + KoordinatengitterStartxVereinfacht);
-
 		} else if (betaiteration > 1) {
 
 			betaVereinfacht = Math.round(betaiteration);
-
 			KoordinatengitterStartxVereinfacht = Math.round(KoordinatengitterStartxiteration);
 
-			System.out.println("minx " + minx);
-			System.out.println("KoordinatengitterStartxVereinfachtx " + KoordinatengitterStartxVereinfacht);
+		}
+
+		// um sicherzugehen, das zwischen dem gewaehlten(und gerundetem wert und dem
+		// startwert keine zu grosse luecke ist wird manuel einen Breitengrad
+		// davorgeschoben
+
+		if (alphaiteration < 1.00) {
+
+			do {
+				alphaiteration = alphaiteration * 10;
+				alphaiterationcounter = alphaiterationcounter + 1;
+
+				// zur vereinfachung wird die zahl gerundet
+				double alphaiterationVereinfacht = Math.round(alphaiteration);
+
+				// und nun wieder in die Ursprungliche dimension umgewandelt
+
+				if (alphaiterationcounter == 0) {
+					alphaVereinfacht = alphaiterationVereinfacht;
+				}
+
+				else {
+					alphaVereinfacht = alphaiterationVereinfacht / Math.pow(10, alphaiterationcounter);
+				}
+
+				// somit wurden zumbeispiel aus 0,337854 Grad jetzt 0.3grad generiert
+
+			} while (alphaiteration <= 1);
+
+			// Ausserdem wird noch ein startwert benoetigt um von diesem die
+			// koordinatenwerte
+			// abzutragen
+
+			KoordinatengitterStartyiteration = KoordinatengitterStartyiteration * Math.pow(10, alphaiterationcounter);
+
+			double iterationKoordinatengitterStartyVereinfacht = Math.round(KoordinatengitterStartyiteration);
+
+			KoordinatengitterStartyVereinfacht = iterationKoordinatengitterStartyVereinfacht
+					/ Math.pow(10, alphaiterationcounter);
+
+		} else if (alphaiteration > 1) {
+
+			alphaVereinfacht = Math.round(alphaiteration);
+			KoordinatengitterStartyVereinfacht = Math.round(KoordinatengitterStartyiteration);
 
 		}
 
@@ -123,30 +170,48 @@ public class Koordinatengitter extends JPanel {
 
 		double ersterKoordinatengitterwertx = KoordinatengitterStartxVereinfacht - (betaVereinfacht / 6);
 		Laengengrade.add(ersterKoordinatengitterwertx);
-		System.out.println("ersterKoordinatengitterwert " + ersterKoordinatengitterwertx);
+
+		double ersterKoordinatengitterwerty = KoordinatengitterStartyVereinfacht - (alphaVereinfacht / 4);
+		Breitengrade.add(ersterKoordinatengitterwerty);
 
 		///////////////////////////////////////////////////////////////////////////////////////////////////
 		// nun werden die anderen Linien erzeugt.
+
+		for (int i = 1; i < 5; i++) {
+			double Koordinatengitterwerty = KoordinatengitterStartyVereinfacht + ((alphaVereinfacht / 4) * i);
+			Breitengrade.add(Koordinatengitterwerty);
+
+		}
+
 		for (int i = 1; i < 7; i++) {
 			double Koordinatengitterwertx = KoordinatengitterStartxVereinfacht + ((betaVereinfacht / 6) * i);
 			Laengengrade.add(Koordinatengitterwertx);
 
 		}
 
+		// jetzt muss allerdings noch die Lage auf dem Bildschirm berechnet werden.
+		/////////// Transformieren //////////
 		for (int j = 0; j < Laengengrade.size(); j++) {
 
 			double Laenge = Laengengrade.get(j);
 			int LaengeBildschirm = (int) (((Laenge - minx) * width) / beta);
 			Laengengradetransformiert.add(LaengeBildschirm);
+			
 		}
 
-		for (int j = 0; j < Laengengradetransformiert.size(); j++) {
-			System.out.println(Laengengradetransformiert.get(j).toString());
+		for (int j = 0; j < Breitengrade.size(); j++) {
+
+			double Breiten = Breitengrade.get(j);
+			int breiteBildschirm = (int) ((( (Math.sqrt(2)-Breiten) - miny) * (width / Math.sqrt(2))) / alpha);
+			Breitengradetransformiert.add(breiteBildschirm);
+			System.out.println("LLLLLLLLLLH+  "+ breiteBildschirm);
+
 		}
-
-		// jetzt muss allerdings noch die lage auf dem Bildschirm berechnet werden.
-
 	}
+
+	//////////////////////// SelbeVorgehensweisefür
+	//////////////////////// die///////////////////////////////
+	////////////////////////////// Breitengrade//////////////////////////////////////
 
 	public void paintComponent(Graphics g) {
 
@@ -171,6 +236,32 @@ public class Koordinatengitter extends JPanel {
 
 		}
 
+	
+	for (int j = 0; j < Breitengradetransformiert.size(); j++) {
+		//System.out.println("HHHHHHHHHHHHHHHHH+  "+Breitengradetransformiert.get(j).toString());
+		
+		int breite = Breitengradetransformiert.get(j);
+		if (breite < 0 || breite > (width/Math.sqrt(2))) {
+
+		} else {
+			g2.drawLine(0, breite, 0, (width));
+
+			// Beschriften der Senkrechten Linien
+
+			String Breitetext = String.valueOf(Math.round(Breitengrade.get(j) * 100) / 100.0);
+			g2.drawString(Breitetext, breite + 10, 10);
+
+		}
+
 	}
+	}
+
+	
+	
+	
+	
+	
+	
+	
 
 }
